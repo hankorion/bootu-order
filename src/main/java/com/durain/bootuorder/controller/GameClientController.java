86369d67d68c18a8c1eaa5d70f81da494276a0fd
hankorion <hankzhang.orion@gmail.com>
2018-10-12 14:21:48 +0800

    add order process

--- src/main/java/com/durain/bootuorder/controller/GameClientController.java
+++ src/main/java/com/durain/bootuorder/controller/GameClientController.java
@@ -1,10 +1,15 @@
 package com.durain.bootuorder.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.durain.bootuorder.client.GameServiceClient;
+import com.durain.bootuorder.dataobject.GameInfo;
+import com.durain.bootuorder.dto.CartDTO;
 
 @RestController
 public class GameClientController {
@@ -15,5 +20,21 @@ public class GameClientController {
 	@GetMapping("/getGameMsg")
 	public String getGameMsg() {
 		return gameServiceClient.gameMsg();
+	}
+
+	@GetMapping("/listFromOrder")
+	public String listFromOrder() {
+
+		List<GameInfo> gameInfoList = gameServiceClient
+				.listFromOrder(Arrays.asList("Blizzard Entertainment - Diablo III"));
+
+		return "" + gameInfoList.size();
+	}
+
+	@GetMapping("/gameDecreaseStock")
+	public String gameDecreaseStock() {
+		CartDTO cardto = new CartDTO("Blizzard Entertainment - Diablo III", 1);
+		gameServiceClient.decreaseGameStock(Arrays.asList(cardto));
+		return "ok";
 	}
 }
--- src/main/java/com/durain/bootuorder/service/impl/OrderServiceImpl.java
+++ src/main/java/com/durain/bootuorder/service/impl/OrderServiceImpl.java
@@ -1,12 +1,21 @@
 package com.durain.bootuorder.service.impl;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.durain.bootuorder.client.GameServiceClient;
+import com.durain.bootuorder.dataobject.GameInfo;
+import com.durain.bootuorder.dataobject.OrderDetails;
 import com.durain.bootuorder.dataobject.OrderMaster;
+import com.durain.bootuorder.dto.CartDTO;
 import com.durain.bootuorder.dto.OrderDTO;
 import com.durain.bootuorder.enums.OrderStatusEnum;
 import com.durain.bootuorder.enums.PayStatusEnum;
@@ -24,17 +33,53 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private OrderDetailsRepository orderDetailsRepository;
 
+	@Autowired
+	private GameServiceClient gameServiceClient;
+
 	@Override
+	@Transactional
 	public OrderDTO createOrder(OrderDTO orderDto) {
-		orderDto.setOrderId(UuidUtil.genUniqueKey());
-		
+
+		String orderId = UuidUtil.genUniqueKey();
+
+		List<String> productIdList = orderDto.getOrderDetailsList().stream().map(OrderDetails::getProductId)
+				.collect(Collectors.toList());
+
+		List<GameInfo> gameInfoList = gameServiceClient.listFromOrder(productIdList);
+
+		BigDecimal orderAmt = new BigDecimal(BigInteger.ZERO);
+		for (OrderDetails orderDtls : orderDto.getOrderDetailsList()) {
+			for (GameInfo gameInfo : gameInfoList) {
+				if (gameInfo.getGameId().equals(orderDtls.getProductId())) {
+					orderAmt = gameInfo.getGamePrice().multiply(new BigDecimal(orderDtls.getProductQuantity()))
+							.add(orderAmt);
+					BeanUtils.copyProperties(gameInfo, orderDtls);
+					orderDtls.setOrderId(orderId);
+					orderDtls.setDetailId(UuidUtil.genUniqueKey());
+					orderDtls.setProductIcon(gameInfo.getGameIcon());
+					orderDtls.setProductId(gameInfo.getGameId());
+					orderDtls.setProductName(gameInfo.getGameName());
+					orderDtls.setProductPrice(gameInfo.getGamePrice());
+					orderDtls.setProductId(gameInfo.getGameId());
+
+					orderDetailsRepository.save(orderDtls);
+				}
+			}
+
+		}
+
+		List<CartDTO> cardDTOList = orderDto.getOrderDetailsList().stream()
+				.map(e -> new CartDTO(e.getProductId(), e.getProductQuantity())).collect(Collectors.toList());
+		gameServiceClient.decreaseGameStock(cardDTOList);
+		orderDto.setOrderId(orderId);
+
 		OrderMaster orderMaster = new OrderMaster();
 		BeanUtils.copyProperties(orderDto, orderMaster);
-		orderMaster.setOrderAmount(new BigDecimal(5));
+		orderMaster.setOrderAmount(orderAmt);
 		orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
 		orderMaster.setPayStatus(PayStatusEnum.NEW.getCode());
 		orderMasterRepository.save(orderMaster);
-		
+
 		return orderDto;
 	}
 
